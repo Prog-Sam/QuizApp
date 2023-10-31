35bedafa6e7b02352e05823e6f06ea3a5065b831
Prog-Sam <bascosam217@gmail.com>
2023-10-31 14:28:46 +0800

    Added small changes

--- App.js
+++ App.js
@@ -1,29 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
-import LoginScreen from './app/screen/LoginScreen';
-import WelcomeScreen from './app/screen/WelcomeScreen';
-import MessagesScreen from './app/screen/MessagesScreen';
+import { useDeviceOrientation } from '@react-native-community/hooks';
 import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading'
+
 import AuthNavigator from './app/navigation/AuthNavigator';
-import RegisterScreen from './app/screen/RegisterScreen';
 import AppNavigator from './app/navigation/AppNavigator';
+import authStorage from './app/auth/storage';
+import AuthContext from './app/auth/context';
 import NavigationTheme from './app/navigation/NavigationTheme';
+import OfflineNotice from './app/components/OfflineNotice';
+import QuizSessionContext from './app/quizSession/context';
+import quizSessionStorage from './app/quizSession/storage'
+import Screen from './app/components/Screen';
+import VideoScreen from './app/screen/VideoScreen';
 
 export default function App() {
+  const [user, setUser] = useState();
+  const [quizSession, setQuizSession] = useState();
+  const [isReady, setIsReady] = useState(false);
+  const isLandscape = useDeviceOrientation() == 'landscape';
+
+  const restoreStates = async () => {
+    const user = await authStorage.getUser();
+    if(user) {
+      setUser(user);
+
+      console.log(user);
+      const localQuizSession = await quizSessionStorage.getLocalQuizSession(user.userId);
+      setQuizSession(localQuizSession);
+    }
+  }
+
+  if(!isReady){
+    return (
+    <AppLoading 
+      startAsync={restoreStates} 
+      onFinish={() => setIsReady(true)} 
+      onError={console.warn}
+    />)
+  }
+  
   return (
-    <GestureHandlerRootView style={styles.container}>
-      <NavigationContainer theme={NavigationTheme}>
-        <AuthNavigator />
-        {/* <AppNavigator /> */}
-      </NavigationContainer>
+    <GestureHandlerRootView style={styles.gestureHandler}>
+      <AuthContext.Provider value={{user, setUser}}>
+        <QuizSessionContext.Provider value={{quizSession, setQuizSession}}>
+          <NavigationContainer theme={NavigationTheme}>
+            <OfflineNotice />
+            {/* <Screen><VideoScreen /></Screen> */}
+            {/* <QuizScreen/> */}
+            {/* <QuestionItemCard onPress={(item) => console.log(item)}/> */}
+            {user ? <Screen><AppNavigator /></Screen> : <AuthNavigator /> }
+          </NavigationContainer>
+        </QuizSessionContext.Provider>
+      </AuthContext.Provider>
     </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  gestureHandler: {
     flex: 1,
   },
 });
---
+++ app/utility/compactTryCatch.js
@@ -0,0 +1,8 @@
+export const compactTryCatch = async (promise) => {
+    try {
+        const data = await promise();
+        return [data, null];
+    } catch (error) {
+        return [null, error];
+    }
+}
